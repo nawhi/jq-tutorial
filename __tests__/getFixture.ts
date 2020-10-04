5ce19b09d7f9ee3f64443647c754a59e0d88f676
nawhi <25394965+nawhi@users.noreply.github.com>
2020-10-04 08:44:06 +0100

    Start to add more functional tests

--- __tests__/getFixture.ts
+++ __tests__/getFixture.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from 'fs';
 import * as path from 'path';
 
-export function getFixture(fixtureFile: string = 'help.txt') {
+export function getFixture(fixtureFile: string) {
   return readFileSync(path.resolve(__dirname, 'fixtures', fixtureFile)).toString();
 }
--- __tests__/jq-tutorial.test.ts
+++ __tests__/jq-tutorial.test.ts
@@ -3,23 +3,42 @@ import { exec } from 'child_process';
 import * as path from 'path';
 import { getFixture } from './getFixture';
 
-const BINARY_PATH = path.resolve(
-  __dirname,
-  '..',
-  'bin',
-  'jq-tutorial'
-);
-
-describe('jq-tutorial acceptance', () => {
+describe('jq-tutorial', () => {
   it('displays help information when run without argument', async () => {
-    const stdout = await new Promise((resolve, reject) => {
-      exec(BINARY_PATH, (err, stdout, stderr) => {
-        if (err) reject(err);
-        console.error(stderr);
-        resolve(stdout);
-      });
-    });
-    const expected = getFixture();
+    const { stdout, stderr } = await runBinary('');
+    expect(stdout).to.eql(getFixture('help.txt'));
+    expect(stderr).to.be.empty;
+  });
+
+  it('displays help information when run with unexpected argument', async () => {
+    const { stdout, stderr } = await runBinary('foo');
+    expect(stdout).to.eql(getFixture('help.txt'));
+    expect(stderr).to.be.empty;
+  });
+
+  xit('displays pick readme and prompt when run with "pick" argument', async () => {
+    const { stdout, stderr } = await runBinary('pick');
+    const expected = getFixture('pick.txt');
     expect(stdout).to.eql(expected);
+    expect(stderr).to.be.empty;
   });
 });
+
+async function runBinary(argument: string): Promise<{ stdout: string, stderr: string }> {
+  const binaryPath = path.resolve(
+    __dirname,
+    '..',
+    'bin',
+    'jq-tutorial'
+  );
+
+  const pathWithArgs = `${binaryPath} ${argument}`;
+
+  return new Promise((resolve, reject) => {
+    exec(pathWithArgs, (err, stdout, stderr) => {
+      if (err) reject(err);
+      console.error(stderr);
+      resolve({ stdout, stderr });
+    });
+  });
+}
