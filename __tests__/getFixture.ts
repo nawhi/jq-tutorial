4cc3ef3b0474d46373a975bcaf1ef6e3ca622608
nawhi <25394965+nawhi@users.noreply.github.com>
2020-11-14 10:59:39 +0000

    Remove old test fixtures

--- __tests__/getFixture.ts
+++
@@ -1,6 +0,0 @@
-import { readFileSync } from 'fs';
-import * as path from 'path';
-
-export function getFixture(fixtureFile: string) {
-  return readFileSync(path.resolve(__dirname, 'fixtures', fixtureFile)).toString();
-}
